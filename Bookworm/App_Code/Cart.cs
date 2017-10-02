082db5b5e27895a4c540d4eb712342935996442c
VerGjor <VerGjor@users.noreply.github.com>
2017-10-02 02:00:28 +0200

    Add files via upload

---
+++ Bookworm/App_Code/Cart.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Cart
+/// </summary>
+public class Cart
+{
+
+    public List<CartItem> Items { get; set; }
+
+    public Cart()
+    {
+        Items = new List<CartItem>();
+    }
+
+
+    public void Add(CartItem item)
+    {
+        Items.Add(item);
+    }
+
+    
+    public void Remove(int rowID)
+    {
+        Items.RemoveAt(rowID);
+    }
+
+    public void Update(int rowID, int Quantity)
+    {
+        if(Quantity > 0 && Quantity <= 10)
+        {
+            Items[rowID].Kolicina = Quantity;
+        }else if (Quantity > 10)
+        {
+            Items[rowID].Kolicina = 10;
+        }else
+        {
+            Remove(rowID);
+        }
+    }
+
+    public int Total {
+        get
+        {
+            if(Items == null)
+            {
+                return 0;
+            }else
+            {
+                int total = 0;
+                WebService webService = new WebService();
+                foreach(CartItem item in Items)
+                {
+                    total = webService.addToSum(item.Cena, item.Kolicina, total);
+                }
+                return total;
+            }
+        }
+    }
+}
---
+++ Bookworm/App_Code/CartItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CartItem
+/// </summary>
+public class CartItem
+{
+
+    public string Slika { get; set; }
+    public string Avtor { get; set; }
+    public string Naslov { get; set; }
+    public int Kolicina { get; set; }
+    public int Cena { get; set; }
+
+    public CartItem()
+    {
+  
+    }
+    
+    public CartItem(string slika, string avtor, string naslov,int cena)
+    {
+        Slika = slika;
+        Avtor = avtor;
+        Naslov = naslov;
+        Kolicina = 1;
+        Cena = cena;
+    }
+}
